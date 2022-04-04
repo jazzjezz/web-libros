df2ff681d568042c2664148bd8a62e207c23f786
jazzjezz <jazzjezz@disroot.org>
2022-04-03 21:30:42 -0500

    Fix navbar books and logout

--- src/assets/components/menu.php
+++ src/assets/components/menu.php
@@ -1,20 +1,23 @@
 <nav class="flex-container bg-white flex-wrap row navShadow nav">
-            <div class="flex-container menuItem row flex-1">
-                <a href="#" class="align-items-center"><span class="material-icons">home</span>Inicio</a>
-            </div>
-            <div class="flex-container menuItem row">
-                <a href="#">Productos</a>                
-            </div>
-            <div class="flex-container menuItem row">
-                <a href="#">Acerca de</a>                
-            </div>
-            <div class="flex-container menuItem row">
-                <a href="#">Iniciar Sesión</a>                
-            </div>
-            <div class="flex-container menuItem row">
-                <a href="#">Registrarse</a>                
-            </div>
-            <div class="flex-container menuItem row">
-                <a href="#"><span class="material-icons">search</span></a>
-            </div>
+    <div class="flex-container menuItem row flex-1">
+        <a href="/index.php" class="align-items-center"><span class="material-icons">home</span>Inicio</a>
+    </div>
+    <div class="flex-container menuItem row">
+        <a href="/pages/books.php">Productos</a>                
+    </div>
+    <div class="flex-container menuItem row">
+        <a href="#">Acerca de</a>                
+    </div>
+    <?php         
+        if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
+            echo '<div class="flex-container menuItem row" disabled><a href="#">' . $_SESSION['user_name'] . '</a></div>';
+            echo '<div class="flex-container menuItem row"><a href="/assets/php/logout.php">Cerrar Sesión</a></div>';
+        } else {
+            echo '<div class="flex-container menuItem row"><a href="/index.php">Iniciar Sesión</a></div>';
+            echo '<div class="flex-container menuItem row"><a href="#">Registrarse</a></div>';
+        }
+    ?>
+    <div class="flex-container menuItem row">
+        <a href="#"><span class="material-icons">search</span></a>
+    </div>
 </nav>
--- src/pages/books.php
+++ src/pages/books.php
@@ -1,3 +1,6 @@
+<?php 
+    session_start(); 
+?>
 <!DOCTYPE html>
 <html lang="es" class="hg-100">
     <head>
